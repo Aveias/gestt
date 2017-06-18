2546c05cb0b3b57aa782da50b3972497423ccec9
Aveias <spam@aveias.fr>
2017-06-18 19:40:07 +0200

    Ajout du modèle Utilisateur

---
+++ Users/Model.py
@@ -0,0 +1,25 @@
+# -*-coding:utf8 -*
+
+import DB.dbLink as db
+
+class User:
+    """Modèle utilisateur. Contient un objet utilisateur"""
+
+    def __init__(self, id):
+        self.identifiant = id
+
+        #On va chercher l'user en base de donn�es
+        link = db.DBLink()
+        query = "SELECT u.IDUtil AS idBDD, u.Nom AS nom, u.Prénom AS prenom, u.Identifiant AS identifiant, u.MdP AS MdP, u.SalaireBrut AS cout, f.Intitulé AS fonction, p.Libelle AS pole FROM utilisateur AS u JOIN fonction AS f ON f.IDFonc = u.IDFonc JOIN pole AS p ON p.IDPole = p.IDPole WHERE u.identifiant = %s"
+        result = link.query(query, [self.identifiant, ])
+
+        #On remplit l'objet
+        for idBDD, fonction, pole, nom, prenom, identifiant, MdP, cout in result:
+            self._id_table = idBDD
+            self.fonction = fonction
+            self.pole = pole
+            self.nom = nom
+            self.prenom = prenom
+            self.identifiant = identifiant
+            self._mdp  = MdP
+            self.cout  = cout
