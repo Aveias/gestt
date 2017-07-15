0fed21d727eed3b24b3bba689db8d5f611f0db73
Aveias <spam@aveias.fr>
2017-07-15 17:48:28 +0200

    Passage de l'utilisateur loggué en utilisateur courant

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import Auth.login as log
 import Users.Model as U
 import os
 
-#On appelle le module d'identification
+#On appelle le module d'identification - Commenté pour les pahses de test d'autres modules
 login = log.Login()
 login.fenetre.mainloop()
 
 #On lance le programme
 while auth.Auth.access == True:
     print("programme en cours")
-    user = U.User('test')
+    user = auth.Auth.current_user
     print("Bonjour", user.nom, user.prenom, "vous êtes dans la boucle")
+
     os.system("pause")
