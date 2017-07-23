5632bad4da8715b30845f145acc10122b610205b
VincentLeduc <vincent.leduc069@gmail.com>
2017-07-23 16:07:20 +0200

    Update barreOutils.py

--- Menu/barreOutils.py
+++ Menu/barreOutils.py
@@ -1,4 +1,4 @@
-#-*-coding:UTF-8-*
+# -*- coding: utf-8 -*-
 
 from tkinter import *
 
@@ -8,10 +8,29 @@ class BarreOutils():
     Tous les widgets sont stockÃ©s comme attributs de cette fenÃªtre."""
     
     def __init__(self):
+	# céation de la fenetre
         self.fenetre = Tk()
+        # Booleen fermer : devient true quand on appuie sur le bouton deconnexion
+        self.fermer = False
+        # Ajout d'un titre
+        self.fenetre.title("Menu")
+        # Dimension et position fenetre
+        self.hauteur = self.fenetre.winfo_screenheight()/1.2
+        self.largeur = self.fenetre.winfo_screenwidth()/10;
+        
+        self.posX = self.fenetre.winfo_screenwidth() - (self.largeur)
+        self.posY = self.fenetre.winfo_screenheight()/2 - (self.hauteur / 2)
+        # empecher le redimensionnement de la fenetre
+        self.fenetre.resizable(width=False, height=False)
+        
+        # convertir la chaine du paramentre de geometry( en liste de parametre entier
+        L,H,X,Y = self.geoliste(self.fenetre.geometry())
+        self.fenetre.geometry("%dx%d%+d%+d" % (self.largeur,self.hauteur,self.posX,self.posY)) # affecter les parametre de dimension et position
+        
+	# Ajout d'un label : titre barre Outils
         self.message = Label(self.fenetre, text="Barre outils")
         self.message.pack()
-        self.fermer = False
+        
 	# Bouton qui ouvre le menu Mes projets :
 	# dans lequel les utilisateurs pourront visualiser les taches et les temps en cours sur le/ les projets en cours
 		
@@ -29,14 +48,19 @@ class BarreOutils():
 	# Bouton Deconnexion
         self.bouton_deconnexion = Button(self.fenetre, text="Deconnexion", command=self.deconnexion)
         self.bouton_deconnexion.pack(side="left")
-		
-		
 
         
     def deconnexion(self):
         """L'utilisateur veut fermer le programme et se deconnecter"""
         self.fenetre.destroy()
         self.fermer = True
+    
+    def geoliste(self,g):
+        r=[i for i in range(0,len(g)) if not g[i].isdigit()]
+        return [int(g[0:r[0]]),int(g[r[0]+1:r[1]]),int(g[r[1]+1:r[2]]),int(g[r[2]+1:])]
+    
+
+
         
 	
 
