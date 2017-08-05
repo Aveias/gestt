42f162b58ddbdc460cf522ad1de2090ecc28c657
Aveias <spam@aveias.fr>
2017-08-05 16:07:19 +0200

    ajout du paramètre multi à l'execute des query

--- DB/dbLink.py
+++ DB/dbLink.py
@@ -16,10 +16,10 @@ class DBLink:
         self._db_connection = mariadb.connect(host = db_ids["host"], user = db_ids["user"], password = db_ids["password"], database = db_ids["database"])
         self._db_cur = self._db_connection.cursor()
 
-    def query(self, query, args):
+    def query(self, query, args, multi=False):
         """query standard"""
         try:
-            self._db_cur.execute(query, args)
+            self._db_cur.execute(query, args, multi)
         except mariadb.Error as e:
             self._db_cur = False
             print("Erreur : ", e)
